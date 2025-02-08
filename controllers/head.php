8538430cc489813ca73b74f804f79ef4eb378602
Sebastian Jimenez <sjimnezc@cesde.net>
2025-02-08 18:50:44 -0500

    Actualiacion de icon para el navegador

--- controllers/head.php
+++ controllers/head.php
@@ -5,7 +5,8 @@
 <meta content="" name="description">
 
 <!-- Favicon -->
-<link href="img/favicon.ico" rel="icon">
+<link href="img/logo_ico.ico" rel="icon">
+<!-- <link href="img/logo.png" rel="icon"> -->
 
 <!-- Google Web Fonts -->
 <link rel="preconnect" href="https://fonts.googleapis.com">
