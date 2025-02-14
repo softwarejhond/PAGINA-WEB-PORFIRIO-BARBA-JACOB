2e6490cd5c1c38b71e6daa988f0c3826e9eb80e5
Sebastian Jimenez <sjimnezc@cesde.net>
2025-02-13 23:04:38 -0500

    Actualizar la plantilla

---
+++ controllers/testimonials.php
@@ -0,0 +1,42 @@
+<div class="container-xxl py-5 wow fadeInUp" data-wow-delay="0.1s">
+    <div class="container">
+        <div class="text-center">
+            <h6 class="section-title bg-white text-center text-primary px-3">Testimonios</h6>
+            <h1 class="mb-5">Nuestros Estudiantes Dicen!</h1>
+        </div>
+        <div class="owl-carousel testimonial-carousel position-relative">
+            <div class="testimonial-item text-center">
+                <img class="border rounded-circle p-2 mx-auto mb-3" src="img/testimonial-1.jpg" style="width: 80px; height: 80px;">
+                <h5 class="mb-0">Nombre del Estudiante</h5>
+                <p>Profesión</p>
+                <div class="testimonial-text bg-light text-center p-4">
+                    <p class="mb-0">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatibus libero dolor quam inventore distinctio, ullam laboriosam maxime soluta voluptas harum.</p>
+                </div>
+            </div>
+            <div class="testimonial-item text-center">
+                <img class="border rounded-circle p-2 mx-auto mb-3" src="img/testimonial-2.jpg" style="width: 80px; height: 80px;">
+                <h5 class="mb-0">Nombre del Estudiante</h5>
+                <p>Profesión</p>
+                <div class="testimonial-text bg-light text-center p-4">
+                    <p class="mb-0">Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda inventore modi quas iste et eos?</p>
+                </div>
+            </div>
+            <div class="testimonial-item text-center">
+                <img class="border rounded-circle p-2 mx-auto mb-3" src="img/testimonial-3.jpg" style="width: 80px; height: 80px;">
+                <h5 class="mb-0">Nombre del Estudiante</h5>
+                <p>Profesión</p>
+                <div class="testimonial-text bg-light text-center p-4">
+                    <p class="mb-0">Lorem ipsum dolor sit amet consectetur adipisicing elit. Non corrupti incidunt voluptates debitis dicta, labore saepe tempore accusantium.</p>
+                </div>
+            </div>
+            <div class="testimonial-item text-center">
+                <img class="border rounded-circle p-2 mx-auto mb-3" src="img/testimonial-4.jpg" style="width: 80px; height: 80px;">
+                <h5 class="mb-0">Nombre del Estudiante</h5>
+                <p>Profesión</p>
+                <div class="testimonial-text bg-light text-center p-4">
+                    <p class="mb-0">Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure a, repudiandae voluptatem obcaecati commodi vel sunt illo rem rerum facilis sapiente consequatur?</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
